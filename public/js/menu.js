2044449e77521cbf8d76741c8e5581b8cbc52d3d
Matilda Brisback <matildabrisback@student-213-218.eduroam.uu.se>
2018-11-19 14:59:44 +0100

    Everything until messaging

---
+++ public/js/menu.js
@@ -0,0 +1,26 @@
+
+var food = [
+  {
+  "name": "Halloumi burger",
+  "kCal": 1000,
+  "lactose": true,
+  "gluten": false,
+  "img": "img/halloumi.jpg"
+  },
+  
+  {
+  "name": "Black bean burger",
+  "kCal": 800,
+  "lactose": false,
+  "gluten": true,
+  "img": "img/blackbean2.jpg"
+  },
+
+  {
+  "name": "Barbeque burger",
+  "kCal": 1400,
+  "lactose": true,
+  "gluten": true,
+  "img": "img/barbeque.jpg"
+  }
+]
---
+++ public/js/vue_script.js
@@ -0,0 +1,53 @@
+var socket = io();
+
+var orders = new Vue({
+  el: '#array',
+  data: {
+    Orderdetails: [],
+    selectedGender: '',
+    selectedPayment: '',
+    checkedBurger: [],
+    losburger: "Choose a burger",
+    menu: food,
+    orders: {},
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+  methods: {
+      markDone: function() {
+          console.log("Button clicked!");
+      },
+
+      getNext: function () {
+        var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+          return Math.max(last, next);
+        }, 0);
+        return lastOrder + 1;
+      },
+      addOrder: function (event) {
+        var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                      y: event.currentTarget.getBoundingClientRect().top};
+        socket.emit("addOrder", { orderId: this.getNext(),
+                                  details: { x: event.clientX - 10 - offset.x,
+                                             y: event.clientY - 10 - offset.y },
+                                  orderItems: ["Beans", "Curry"]
+                                });
+      }
+      /*displayOrder: function(event) {var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                                                  y: event.currentTarget.getBoundingClientRect().top};
+      socket.emit("addOrder", { orderId: "T",
+                                details: { x: event.clientX - 10 - offset.x,
+                                           y: event.clientY - 10 - offset.y },
+                                orderItems: ["Beans", "Curry"]
+                              });
+
+  }*/
+}
+});
